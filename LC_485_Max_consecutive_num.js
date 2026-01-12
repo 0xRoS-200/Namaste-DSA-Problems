a86821e608cf7fe01629985a123922745445d1ab
0xRoS-200 <official.rohitsingh22@gmail.com>
2026-01-12 06:40:22 +0530

    Feat: Added LC 268 & 485

---
+++ LC_485_Max_consecutive_num.js
@@ -0,0 +1,19 @@
+var findMaxConsecutiveOnes = function (nums) {
+  let maxCount = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      maxCount = Math.max(maxCount, count); 
+      count = 0;
+    } else {
+      count++;
+    }
+  }
+  
+  return Math.max(maxCount, count);
+};
+
+const nums = [1, 1, 1, 0, 1, 0];
+const result = findMaxConsecutiveOnes(nums);
+console.log(result); // Output: 3
